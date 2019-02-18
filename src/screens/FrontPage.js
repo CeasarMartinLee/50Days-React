64ae473f173f7666695e4a2778b80bf76ea8e85a
Mayron <mvanleenden@gmail.com>
2019-02-18 12:55:11 +0100

    Setup redux & router

---
+++ src/screens/FrontPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FrontPage extends Component {
+  render() {
+    return(
+      <div>
+      <p>FRONT PAGE</p>
+      </div>
+    )
+  }
+}
+
+export default FrontPage
