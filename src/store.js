2ec087550f94feb83317ae7d6804326261bfb166
Mayron <mvanleenden@gmail.com>
2019-02-19 15:40:34 +0100

    small fix

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import reducer from './reducers'
 import ReduxThunk from 'redux-thunk'
 
 
-const enhancer;
+const enhancer = null;
 
 if(process.env.NODE_ENV === 'production') {
   const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
