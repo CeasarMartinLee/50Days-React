d3a4d861580c6fc38b33060424fb174b1624950c
Mayron <mvanleenden@gmail.com>
2019-02-19 15:38:13 +0100

    change store enhancer

--- src/store.js
+++ src/store.js
@@ -2,12 +2,20 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import ReduxThunk from 'redux-thunk'
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const enhancer = compose(
-  applyMiddleware(ReduxThunk),
-  devTools
-)
+const enhancer;
+
+if(process.env.NODE_ENV === 'production') {
+  const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+  enhancer = compose(
+    applyMiddleware(ReduxThunk),
+    devTools
+  )
+} else {
+  enhancer = compose(ReduxThunk)
+}
+
 const store = createStore(reducer, enhancer)
 
 export default store
